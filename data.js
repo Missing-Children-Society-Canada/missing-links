c57a5e7668b2f02ee2e97e39fca9bb4fa751481d
jef king <jefkin@microsoft.com>
2018-10-30 14:26:42 -0700

    fix angular

--- data.js
+++ data.js
@@ -1,14 +1,24 @@
 {
-    "people": [
+    "people" [
         {
-            "photo":"",
-            "name":"Jef King",
-            "link":"",
-            "age":"22",
-            "height":"130",
-            "weight":"130",
-            "attri":"brown",
-            "sex":"woman"
+            "photo": "",
+            "name": "Jef King",
+            "link": "",
+            "age": "22",
+            "height": "190",
+            "weight": "130",
+            "attri": "brown",
+            "sex": "woman"
+        },
+        {
+            "photo": "",
+            "name": "Meta King",
+            "link": "",
+            "age": "4",
+            "height": "22",
+            "weight": "130",
+            "attri": "green",
+            "sex": "man"
         }
     ]
 }
