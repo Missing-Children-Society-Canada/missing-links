8fe22b09cce16397ff7c6add707df6b3f5b63d27
jef king <jefkin@microsoft.com>
2018-10-30 14:04:51 -0700

    further

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var missingApp = angular.module('missingApp', []);
+
+missingApp.controller('missing', ['$scope', '$http', function ($scope, $http) {
+    $http.get('/data.js').success(function (data) {
+        $scope.peoples = data;
+
+        angular.forEach($scope.peoples, function (person) {
+            person.selected = false;
+        });
+    });
+}]);
---
+++ data.js
@@ -0,0 +1,14 @@
+{
+    "people": [
+        {
+            "photo":"",
+            "name":"Jef King",
+            "link":"",
+            "age":"22",
+            "height":"130",
+            "weight":"130",
+            "attri":"brown",
+            "sex":"woman"
+        }
+    ]
+}
