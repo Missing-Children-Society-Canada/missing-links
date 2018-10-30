b43f52613e56cca9d98a38fa0bd468bb902823b6
jef king <jefkin@microsoft.com>
2018-10-30 14:30:11 -0700

    remove loop

--- app.js
+++ app.js
@@ -5,9 +5,5 @@ var missingApp = angular.module('missingApp', []);
 missingApp.controller('missing', ['$scope', '$http', function ($scope, $http) {
     $http.get('/data.js').then(function (data) {
         $scope.peoples = data;
-
-        angular.forEach($scope.peoples, function (person) {
-            person.selected = false;
-        });
     });
 }]);
