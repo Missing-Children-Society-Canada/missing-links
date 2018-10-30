a16311fb0dcfbf7a5d45b305847da26d025a9915
jef king <jefkin@microsoft.com>
2018-10-30 15:43:25 -0700

    naming

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var missingApp = angular.module('missingApp', []);
 
 missingApp.controller('missing', ['$scope', '$http', function ($scope, $http) {
     $http.get('https://mcscstorage.blob.core.windows.net/tweets/tweets.json').then(function (data) {
-        $scope.peoples = data.data;
+        $scope.tweets = data.data;
 
         angular.forEach(data.data, function (person) {
             // $http.post('https://unkown.com/api/endpoint', { "uri": person.link }).then(function (details) {
