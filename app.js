1afaa5d200ec00fe61356f2b0628399375f5ccd1
jef king <jefkin@microsoft.com>
2018-10-30 15:07:42 -0700

    inner loop

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ var missingApp = angular.module('missingApp', []);
 missingApp.controller('missing', ['$scope', '$http', function ($scope, $http) {
     $http.get('/data.js').then(function (data) {
         $scope.peoples = data.data.peoples;
+
+        angular.forEach($scope.peoples, function (person) {
+            $http.post('https://unkown.com/api/endpoint', { "uri": person.link }).then(function (details) {
+                person.details = details;
+            });
+        });
     });
 }]);
