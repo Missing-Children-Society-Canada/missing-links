d6637a54841a23072402b1468ec210efa5d5f34b
jef king <jefkin@microsoft.com>
2018-10-30 14:48:05 -0700

    logging

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var missingApp = angular.module('missingApp', []);
 missingApp.controller('missing', ['$scope', '$http', function ($scope, $http) {
     $http.get('/data.js').then(function (data) {
         $scope.peoples = data.peoples;
+        console.log(data);
+        console.log($scope.peoples);
         angular.forEach($scope.peoples, function (person) {
             console.log(person.name);
         };
