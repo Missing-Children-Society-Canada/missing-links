7791fe2677395c3b0c2c383295eac01fe0388188
jef king <jefkin@microsoft.com>
2018-10-30 15:38:52 -0700

    fixes

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ missingApp.controller('missing', ['$scope', '$http', function ($scope, $http) {
     $http.get('https://mcscstorage.blob.core.windows.net/tweets/tweets.json').then(function (data) {
         $scope.peoples = data.data;
 
-        console.log(data);
-        console.log(scope.peoples);
-        
-        angular.forEach($scope.peoples, function (person) {
+        angular.forEach(data.data, function (person) {
             // $http.post('https://unkown.com/api/endpoint', { "uri": person.link }).then(function (details) {
             //     person.details = details;
             // });
