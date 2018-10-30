6bcc75d8c8535aad4a3d787904913dbe0f088880
jef king <jefkin@microsoft.com>
2018-10-30 15:20:08 -0700

    hooked up

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var missingApp = angular.module('missingApp', []);
 
 missingApp.controller('missing', ['$scope', '$http', function ($scope, $http) {
-    $http.get('/data.js').then(function (data) {
+    $http.get('https://mcscstorage.blob.core.windows.net/tweets/tweets.json').then(function (data) {
         $scope.peoples = data.data.peoples;
 
         angular.forEach($scope.peoples, function (person) {
